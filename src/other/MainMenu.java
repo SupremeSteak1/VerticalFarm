576d3a8f19a61897b83e3683c389dcf783f1dbc3
18GaoA <18GaoA@NSBORO.K12.MA.US>
2016-06-08 11:52:49 -0400

    Added Functions
    Added functions to MainMenu buttons and also added the actual picture of
    the menu.

--- src/other/MainMenu.java
+++ src/other/MainMenu.java
@@ -9,13 +9,17 @@ import engine.input.Mouse;
 
 public class MainMenu implements GameObject {
 
+	ArrayList<Renderable> toRender;
+	boolean q;
+
 	@Override
 	public ArrayList<Renderable> render() {
-
-		RenderableImage mm = new RenderableImage("res/mainmenu-hd.png", 0, 0, 1);
-
-		ArrayList<Renderable> toRender = new ArrayList<>();
-		toRender.add(mm);
+		toRender = new ArrayList<>();
+		
+		if(q == false) {
+			RenderableImage mm = new RenderableImage("res/mainmenu-hd.png", 0, 0, 1);
+			toRender.add(mm);
+		}
 
 		return toRender;
 	}
@@ -23,16 +27,16 @@ public class MainMenu implements GameObject {
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-		
+
 		double x = Mouse.getRecentClickLocationOnScreen().getX();
 		double y = Mouse.getRecentClickLocationOnScreen().getY();
 		// start button
 		if(x >= 240 && x <= 375 && y >= 811 && y <= 872) {
-			System.out.println("START");
+			q = true;
 		}
 		// exit button
 		if(x >= 632 && x <= 759 && y >= 817 && y <= 886) {
-			System.out.println("EXIT");
+			System.exit(0);
 		}
 	}
 
